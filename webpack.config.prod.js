b611ee48043e62b3a75e826d52ff718f1d216ba3
Filip Rajec <filip@reusably.com.au>
2024-05-13 13:17:54 +1000

    updated prod deployment

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, "src", "index.tsx"),
   output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
   mode: process.env.NODE_ENV || "production",
   resolve: {
